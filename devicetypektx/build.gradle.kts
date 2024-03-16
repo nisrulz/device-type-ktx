c16d606b3b1c079227bebe5953137cd45df32934
Nishant Srivastava <nisrulz@gmail.com>
2024-03-16 14:59:03 +0100

    wire in the publishing plugin

--- devicetypektx/build.gradle.kts
+++ devicetypektx/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+
+    id("com.vanniktech.maven.publish")
 }
 
 android {
@@ -33,10 +35,10 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
