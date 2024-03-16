99685a7682d398b3b2af73d55fe151eb55d8bfc5
Nishant Srivastava <nisrulz@gmail.com>
2024-03-17 00:12:02 +0100

    updated sample app

--- app/src/main/java/com/github/nisrulz/devicetypektxproject/MainActivity.kt
+++ app/src/main/java/com/github/nisrulz/devicetypektxproject/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.github.nisrulz.devicetypektxproject
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.nisrulz.devicetypektx.isLandscapeOrientation
 import com.github.nisrulz.devicetypektx.isPhone
 import com.github.nisrulz.devicetypektx.isPortraitOrientation
 import com.github.nisrulz.devicetypektx.isTablet
@@ -19,18 +29,7 @@ import com.github.nisrulz.devicetypektxproject.ui.theme.DeviceTypeKTXProjectThem
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
         lockToPortraitOrientation(phone = true)
-
-        if (isPortraitOrientation()) {
-            println(">> Portrait orientation requested")
-        }
-
-        if (isPhone()) {
-            println(">> Device is a Phone")
-        } else if (isTablet()) {
-            println(">> Device is a Tablet")
-        }
 
         setContent {
             DeviceTypeKTXProjectTheme {
@@ -39,26 +38,61 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val txtToShow = prepareStringToShow()
+                    MainScreen(txtToShow)
                 }
             }
         }
     }
 }
 
+private fun Activity.prepareStringToShow(): String {
+    val txtToShow = StringBuilder().append("Device Type = ")
+
+    if (isPhone()) {
+        txtToShow.append("Phone")
+    } else if (isTablet()) {
+        txtToShow.append("Tablet")
+    }
+
+    txtToShow
+        .append("\n")
+        .append("Orientation Set = ")
+
+    if (isPortraitOrientation()) {
+        txtToShow.append("Portrait")
+    } else if (isLandscapeOrientation()) {
+        txtToShow.append("Landscape")
+    } else {
+        txtToShow.append("No")
+    }
+    return txtToShow.toString()
+}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(text: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier.size(300.dp),
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo"
+        )
+        Text(
+            text = text,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     DeviceTypeKTXProjectTheme {
-        Greeting("Android")
+        MainScreen("Hello World")
     }
 }
