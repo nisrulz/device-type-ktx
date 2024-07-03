acf143c727272f6c9ae2469dfe0bd31d9389ee53
Nishant Srivastava <nisrulz@users.noreply.github.com>
2024-07-04 00:18:18 +0200

    Setup Binary Compatibility Validator plugin for API Checks (#18)
    * setup binary compatibility validator plugin
    
    * updated library to latest version
    
    * setup baseline api
    
    * updated dev docs
    
    * add Github Action for pre merge checks
    
    * make github actions work when targeting develop branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    // Android
     alias(libs.plugins.android.application) apply false
+
     alias(libs.plugins.android.library) apply false
 
-    // Kotlin
     alias(libs.plugins.kotlin.android) apply false
 
-    // Maven Publishing
     alias(libs.plugins.maven.publish) apply false
 
-    // Dokka for Documentation
     alias(libs.plugins.dokka) apply false
 
-    // Compose
     alias(libs.plugins.compose.compiler) apply false
+
+    alias(libs.plugins.binary.compatibility.validator) apply false
 }
 
 //region Publishing Tasks
