e3a8ae3b8dbb28dd95e3ac9a3f015adfd7ee0183
Nishant Srivastava <nisrulz@gmail.com>
2024-07-03 23:01:27 +0200

    switch jvm target to 17

--- build-logic/src/main/kotlin/AndroidConfig.kt
+++ build-logic/src/main/kotlin/AndroidConfig.kt
@@ -2,7 +2,7 @@ object BuildSdkInfo {
     const val COMPILE_SDK_VERSION = 34
     const val MIN_SDK_VERSION = 21
     const val TARGET_SDK_VERSION = 34
-    const val JVM_TARGET = 8
+    const val JVM_TARGET = 17
 }
 
 object ApplicationInfo {
