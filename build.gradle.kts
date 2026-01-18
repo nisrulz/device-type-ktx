789be2998e04d7f4de1de43edbe3d1618e35832a
Nishant Srivastava <nisrulz@gmail.com>
2026-01-19 00:31:51 +0100

    switch to agp 9.0.0 + builtin kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
 
     alias(libs.plugins.android.library) apply false
 
-    alias(libs.plugins.kotlin.android) apply false
-
     alias(libs.plugins.maven.publish) apply false
 
     alias(libs.plugins.dokka) apply false
